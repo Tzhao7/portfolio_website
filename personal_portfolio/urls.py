1793a667226c491f23bd37599ef1e03b8999d0de
Tzhao7 <tzhao625@gmail.com>
2021-01-13 14:25:12 -0500

    Redirect View

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('home/', include('home.urls')),
+    path('', RedirectView.as_view(url='home/')),
 ]
+
+
