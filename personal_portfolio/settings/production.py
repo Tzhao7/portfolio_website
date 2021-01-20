bf9162cef1c1ee9a7b46e1a6f603f983712d343c
Tzhao7 <tzhao625@gmail.com>
2021-01-20 08:42:54 -0500

    Test_3

--- personal_portfolio/settings/production.py
+++ personal_portfolio/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 import os 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = os.environ.get('DJANGO_DEBUG')
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['terryzhao.herokuapp.com']
